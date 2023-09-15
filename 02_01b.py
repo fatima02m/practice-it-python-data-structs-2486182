a4e63d35f18343015fec8d7d484ccad6f93ee3d1
fatima02m <95159942+fatima02m@users.noreply.github.com>
2023-09-15 23:11:51 +0000

    maxlen deque practice

--- 02_01b.py
+++ 02_01b.py
@@ -1,7 +1,23 @@
+from collections import deque
+
 
 def main():
-    #add code here
+    # creating a deque size 5
+    foods = deque(maxlen=5)
+    # appends to the right side of the deque
+    foods.append("STA001")
+    ordered_foods = ["DES003", "STA002", "ENT004", "ENT001"]
+    foods.extend(ordered_foods)
+
+    # adding the 6th food will remove the first move so you have max 5
+    foods.append("DES002")
+
+   # appending to the left
+    foods.appendleft("DES005")
+
+    print(foods)
     return
+
 
 if __name__ == "__main__":
     main()
